be20fa1636d1d1a996a082242a78e7b8fed96f4d
mtnleo <mtnleonardi@gmail.com>
2025-05-28 21:46:32 -0300

    Last API modif

--- back-prep/api.js
+++ back-prep/api.js
@@ -4,13 +4,20 @@ const server = http.createServer((req, res) => {
     console.log("Req received.");
 
     if(req.url === '/' && req.method === "GET") {
-        res.setHeader('Content-Type', 'text-html')
+        res.writeHead(200, {'Content-Type': 'text-html'})
         res.end('<h1>Welcome<h1>')
-    }
-
-    if(req.url === '/' && req.method === "POST") {
-        res.setHeader('Content-Type', 'text-html')
-        res.end('<h1>You dont have permission to do that<h1>')
+    } else if(req.url === '/' && req.method === "POST") {
+        res.writeHead(401, {'Content-Type': 'text-html'})
+        res.end('<h1>You don`t have permission to do that!<h1>')
+    } else if (req.url === '/about' && req.method === "GET") {
+        res.writeHead(200, {'Content-Type': 'text-html'})
+        res.end('<h1>About<h1>')
+    } else if (req.url === '/contact') {
+        res.writeHead(200, {"Content-type": "text-html"});
+        res.end("<h1>Contact Us<h1> <p>Please fill out this form<p>")
+    } else {
+        res.writeHead(404, {"Content-type": "text-html"});
+        res.end("<h1>404 Not Found :(<h1>")
     }
 
 
